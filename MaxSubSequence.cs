8efec151c5b4d671614c1292828a953c08fe34bf
Jonater <s919student@gmail.com>
2016-02-27 13:23:22 +0800

    最大子段和，是求最大子矩阵的一个子问题

---
+++ MaxSubSequence.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace leetcode
+{
+    class MaxSubSequence
+    {
+        public int MaxSubSeq(int[] nums)
+        {
+            int MaxSum = 0;
+            int NowSum = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                NowSum += nums[i];
+                if (NowSum > MaxSum) { MaxSum = NowSum; }
+                if (NowSum < 0) { NowSum = 0; }
+            }
+            return NowSum;
+        }
+    }
+}
