cbc460045a6f2f94e66aa04285c35fb278817dcd
Jonater <s919student@gmail.com>
2016-03-02 21:55:05 +0800

    code

---
+++ RotateArray.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace leetcode
+{
+    class RotateArray
+    {
+        public void Rotate(int[] nums, int k)
+        {
+            int length = nums.Length;
+            k %= length;
+            int[] tail = new int[k];
+            for (int i = 0; i < k; i++)
+            {
+                tail[i] = nums[length - k + i];
+            }
+            for (int i = length - 1 - k; i >= 0; i--)
+            {
+                nums[i + k] = nums[i];
+            }
+            for (int i = 0; i < k; i++)
+            {
+                nums[i] = tail[i];
+            }
+        }
+    }
+}
