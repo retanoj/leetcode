65bb11973d5b0c18ef3d55f2acc72e80e25f2964
Jonater <s919student@gmail.com>
2016-03-02 09:46:33 +0800

    code

--- KthSmallestInBST.cs
+++ KthSmallestInBST.cs
@@ -11,7 +11,7 @@ namespace leetcode
             public int val;
             public TreeNode left;
             public TreeNode right;
-            public TreeNode(int x) { val = x; }
+            public TreeNode(int x) { val = x; left = null; right = null; }
         }
         public int KthSmallest(TreeNode root, int k)
         {
