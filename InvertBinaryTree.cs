f0c221e22cf3a164a8937c3f41df0462d6fe26d7
Jonater <s919student@gmail.com>
2016-03-02 09:47:09 +0800

    反转二叉树

---
+++ InvertBinaryTree.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace leetcode
+{
+    class InvertBinaryTree
+    {
+         public class TreeNode {
+             public int val;
+             public TreeNode left;
+             public TreeNode right;
+             public TreeNode(int x) { val = x; }
+         }
+        TreeNode invert(TreeNode r1, TreeNode r2)
+        {
+            if (r1 == null) { return null; }
+            r2 = new TreeNode(r1.val);
+
+            r2.right = invert(r1.left, r2.right);
+            r2.left = invert(r1.right, r2.left);
+            return r2;
+        }
+        public TreeNode InvertTree(TreeNode root)
+        {
+            TreeNode root2 = invert(root, null);
+            return root2;
+        }
+    }
+}
