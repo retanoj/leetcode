76f125716ceefae1bc402283fb453583f75d2c07
Jonater <s919student@gmail.com>
2016-03-11 10:50:09 +0800

    二分查找

--- BinarySearchCls.cs
+++ BinarySearchCls.cs
@@ -7,6 +7,8 @@ namespace leetcode
 {
     class BinarySearchCls
     {
+        //init x=0, y=nums.Length
+
         //返回第一个出现的位置，或应插入的位置
         public int binSearch_lowerBound(int[] nums, int goal, int x, int y)
         {
@@ -32,20 +34,14 @@ namespace leetcode
 
         public int binSearch(int[] nums, int goal, int x, int y)
         {
-            while (x <= y)
+            while(x<y)
             {
                 int mid = x + (y - x) / 2;
                 if (goal == nums[mid]) { return mid; }
-                else if (goal < nums[mid]) { y = mid - 1; }
+                else if (goal < nums[mid]) { y = mid; }
                 else { x = mid + 1; }
             }
             return -1;
         }
-        
-        public int BinarySearch(int[] nums, int goal)
-        {
-            return binSearch(nums, goal, 0, nums.Length-1);
-        }
-
     }
 }
